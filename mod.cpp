9faf6fbb58a4d945f2510caadcf6873c6562cd09
abhiram2600 <abhiram2600@users.noreply.github.com>
2020-04-13 17:13:17 +0530

    problems

---
+++ mod.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define endl "\n"
+#define f(n) for(int i=0;i<int(n);i++)
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        if(m%n==0)
+        cout<<"Yes"<<endl;
+        else
+        cout<<"No"<<endl;
+        
+    }
+}
